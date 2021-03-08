9492ab36aecad5831fe49754f09486f836f64436
prondubuisi <onyemenamndu@gmail.com>
2021-03-08 02:23:57 +0100

    Add maker bundle, routes annotation and hello controller

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function index(): Response
+    {
+        usleep(30000);
+        
+        return new Response('Hello World');
+    }
+}
